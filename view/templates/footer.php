fe79b712689bdbb5012e46f27ddc82f1225a7a57
Daniel Chung <danielvchung@hotmail.com>
2019-12-19 17:13:27 -0300

    navbar done

--- view/templates/footer.php
+++ view/templates/footer.php
@@ -10,13 +10,6 @@
     <!-- JS -->
     <script> // Logo Name 'FranBeauty'
         var windowWidth = window.innerWidth;
-        var windowHeight = window.innerHeight;
-        var screenWidth = screen.width;
-        var screenHeight = screen.height;
-        console.log('windowWidth = ',windowWidth);
-        console.log('windowHeight = ',windowHeight);
-        console.log('screenWidth = ',screenWidth);
-        console.log('screenHeight = ',screenHeight);
 
         window.onscroll = function() {
             (windowWidth >= 1200) ? scrollFunction(30, 150) : scrollFunction(30, 52);
@@ -38,8 +31,46 @@
             (status) ? glow.classList.add('glow-lg') : glow.classList.remove('glow-lg');
         }
     </script>
-    <script> // Line Page Name
-        console.log(document.querySelectorAll('.page'));
+    <script> // Animation of name bar navigation line
+        var path = window.location.pathname;
+        var pages = document.querySelectorAll('.page');
+
+        pages.forEach(function(page) {
+            var target = page.parentElement;
+            target.addEventListener('mouseover', function() {
+                page.classList.add('page-active');
+            });
+            target.addEventListener('mouseout', function() {
+                page.classList.remove('page-active');
+            });
+        });
+
+        function verifyPath(path) {
+            switch (path) {
+                case '/':
+                    currentPage(0);
+                    break;
+                case '/view/service.php':
+                    currentPage(1);
+                    break;
+                case '/view/about.php':
+                    currentPage(2);
+                    break;
+                case '/view/jobs.php':
+                    currentPage(3);
+                    break;
+                case '/view/contact.php':
+                    currentPage(4);
+                    break;
+            }
+        }
+
+        function currentPage(index) {
+            pages[index].parentElement.classList.add('active');
+            pages[index].classList.add('page-current');
+        }
+
+        verifyPath(path);
     </script>
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
--- view/templates/navbar.php
+++ view/templates/navbar.php
@@ -1,20 +1,17 @@
 <header>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-        <a class="navbar-brand glow glow-lg" href="#">
-            <!-- <img class="visitor-card" src="img/avatar-fran.jpeg" width="30" height="30" alt="Cartão de visita."> -->
-            FranBeauty
-        </a>
+        <a class="navbar-brand glow glow-lg" href="#">FranBeauty</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
             aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
             <div class="navbar-nav">
-                <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span><div class="page"></div></a>
-                <a class="nav-item nav-link" href="#">Serviços<div class="page"></div></a>
-                <a class="nav-item nav-link" href="#">Quem sou<div class="page"></div></a>
-                <a class="nav-item nav-link" href="#">Portfólio<div class="page"></div></a>
-                <a class="nav-item nav-link" href="#">Contato<div class="page"></div></a>
+                <a class="nav-item nav-link" href="/">Home<div class="page"></div></a>
+                <a class="nav-item nav-link" href="../view/service.php">Serviços<div class="page"></div></a>
+                <a class="nav-item nav-link" href="../view/about.php">Quem sou<div class="page"></div></a>
+                <a class="nav-item nav-link" href="../view/jobs.php">Portfólio<div class="page"></div></a>
+                <a class="nav-item nav-link" href="../view/contact.php">Contato<div class="page"></div></a>
             </div>
         </div>
     </nav>
