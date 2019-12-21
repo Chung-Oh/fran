1f0dcabd7071409b40745b806a4c1070073820bf
Daniel Chung <danielvchung@hotmail.com>
2019-12-20 23:53:38 -0300

    init section jobs

--- view/index/jobs.php
+++ view/index/jobs.php
@@ -2,7 +2,32 @@
     <div id="jobsImg" class="parallax"></div>
     <h2 id="jobs" class="name-section text-center py-3">Portfólio <span class="dot">.</span></h2>
     <div class="container">
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-        <img class="img-fluid" src="img/material/sobrancelha.jpg" alt="titulo sobre imagem">
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+            industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+            scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into
+            electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of
+            Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like
+            Aldus PageMaker including versions of Lorem Ipsum.</p>
     </div>
+
+    <!-- Trigger the Modal -->
+    <img id="myImg" src="img/jobs/design-com-hena.jpeg" alt="Design com hena" style="width:100%;max-width:300px">
+    <img id="myImg" src="img/jobs/make-noiva.jpeg" alt="Make noite" style="width:100%;max-width:300px">
+    <img id="myImg" src="img/jobs/cabelo-laque.jpeg" alt="Cabelo laque" style="width:100%;max-width:300px">
+    <img id="myImg" src="img/jobs/make-curso-expert-em-noivas.jpeg" alt="Make curso expert em noivas" style="width:100%;max-width:300px">
+    <img id="myImg" src="img/jobs/make-glan.jpeg" alt="Make glan" style="width:100%;max-width:300px">
+    <img id="myImg" src="img/jobs/micropgmentacao.jpeg" alt="Micropgmentação" style="width:100%;max-width:300px">
+    <img id="myImg" src="img/jobs/microblading-fio-a-fio.jpeg" alt="Microblanding fio a fio" style="width:100%;max-width:300px">
+    <img id="myImg" src="img/jobs/penteado-para-festa.jpeg" alt="Penteado para festa" style="width:100%;max-width:300px">
+    <img id="myImg" src="img/jobs/make-pele-negra.jpeg" alt="Make pele negra" style="width:100%;max-width:300px">
+    <!-- The Modal -->
+    <div id="myModal" class="modal">
+        <!-- The Close Button -->
+        <span class="close">&times;</span>
+        <!-- Modal Content (The Image) -->
+        <img class="modal-content" id="img01">
+        <!-- Modal Caption (Image Text) -->
+        <div id="caption"></div>
+    </div>
+
 </section>
