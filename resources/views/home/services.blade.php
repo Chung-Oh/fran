f01dda703a2bdc787edff53d28e3f0da085bd840
Daniel Chung <danielvchung@hotmail.com>
2019-12-23 20:57:28 -0300

    new services on home page

--- resources/views/home/services.blade.php
+++ resources/views/home/services.blade.php
@@ -2,20 +2,35 @@
     <div id="servicesImg" class="parallax"></div>
     <h2 id="services" class="name-section text-center py-3">Serviços <span class="dot">.</span></h2>
     <div class="container row mx-auto py-3 text-center">
-        <div class="py-3 col-xl-4 col-lg-4 col-md-4">
+        <div class="py-3 col-xl-4 col-lg-4 col-md-6 col-sm-6 col-12">
             <h6 class="service-type pt-2">Casamento</h6>
             <div class="line"></div>
             <p class="content font-italic pb-2">Colocar aqui sobre serviços para casamentos. Colocar aqui sobre serviços para casamentos. Colocar aqui sobre serviços para casamentos. </p>
         </div>
-        <div class="py-3 col-xl-4 col-lg-4 col-md-4">
+        <div class="py-3 col-xl-4 col-lg-4 col-md-6 col-sm-6 col-12">
             <h6 class="service-type pt-2">Debutante</h6>
             <div class="line"></div>
             <p class="content font-italic pb-2">Colocar aqui sobre serviços para debutantes. Colocar aqui sobre serviços para debutantes. Colocar aqui sobre serviços para debutantes. </p>
         </div>
-        <div class="py-3 col-xl-4 col-lg-4 col-md-4">
-            <h6 class="service-type pt-2">Festas</h6>
+        <div class="py-3 col-xl-4 col-lg-4 col-md-6 col-sm-6 col-12">
+            <h6 class="service-type pt-2">Eventos em geral</h6>
             <div class="line"></div>
-            <p class="content font-italic pb-2">Colocar aqui sobre serviços para festas. Colocar aqui sobre serviços para festas. Colocar aqui sobre serviços para festas. </p>
+            <p class="content font-italic pb-2">Colocar aqui sobre serviços para Eventos em geral. Colocar aqui sobre serviços para Eventos em geral. Colocar aqui sobre serviços para festas. </p>
+        </div>
+        <div class="py-3 col-xl-4 col-lg-4 col-md-6 col-sm-6 col-12">
+            <h6 class="service-type pt-2">Make balada</h6>
+            <div class="line"></div>
+            <p class="content font-italic pb-2">Colocar aqui sobre serviços para Make balada. Colocar aqui sobre serviços para Make balada. Colocar aqui sobre serviços para Make balada. </p>
+        </div>
+        <div class="py-3 col-xl-4 col-lg-4 col-md-6 col-sm-6 col-12">
+            <h6 class="service-type pt-2">Make social</h6>
+            <div class="line"></div>
+            <p class="content font-italic pb-2">Colocar aqui sobre serviços para Make social. Colocar aqui sobre serviços para Make social. Colocar aqui sobre serviços para Make social. </p>
+        </div>
+        <div class="py-3 col-xl-4 col-lg-4 col-md-6 col-sm-6 col-12">
+            <h6 class="service-type pt-2">Make ensaio fotográfico</h6>
+            <div class="line"></div>
+            <p class="content font-italic pb-2">Colocar aqui sobre serviços para Make ensaio fotográfico. Colocar aqui sobre serviços para Make ensaio fotográfico. Colocar aqui sobre serviços para Make ensaio fotográfico. </p>
         </div>
     </div>
     <div class="container text-center py-5 mb-5">
