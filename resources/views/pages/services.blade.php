fe8ccda4393ec48836bc2924dd6c370ccf04c861
Daniel Chung <danielvchung@hotmail.com>
2019-12-24 14:19:24 -0300

    improving home and services page

--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -8,9 +8,8 @@
         <div id="servicesImgPage" class="parallax">
             <h1 class="title-jumbo-secondary">Meus Serviços</h1>
         </div>
-        <div class="container">
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        <div class="container py-3">
+            <p class="content">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
             <img class="img-fluid" src="../img/material/batons.jpg" alt="titulo sobre imagem">
         </div>
     </section>
