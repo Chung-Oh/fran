713155b68abba2f06b36034d0cc5cbd41c8ab851
Daniel Chung <danielvchung@hotmail.com>
2019-12-24 15:01:13 -0300

    jumbotron about and jobs page

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -4,11 +4,13 @@
 
 @section('content')
 
-    <section style="margin-top: 400px">
-        <div>
-            <h1>Sobre mim</h1>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-            <img class="img-fluid" src="../img/fran/fran.jpg" alt="titulo sobre imagem">
+    <section>
+        <div id="aboutImgPage" class="parallax">
+            <h1 class="title-jumbo-secondary">Sobre mim</h1>
+        </div>
+        <div class="container py-3">
+            <p class="content">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            <img class="img-fluid" src="../img/professional/francielly-rodrigues-11.jpg" alt="titulo sobre imagem">
         </div>
     </section>
 
