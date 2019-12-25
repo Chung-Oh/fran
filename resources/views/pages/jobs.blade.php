8bce0479b086253dbc532f9890c82ff982d4fdf8
Daniel Chung <danielvchung@hotmail.com>
2019-12-25 17:28:37 -0300

    jobs page almost done

--- resources/views/pages/jobs.blade.php
+++ resources/views/pages/jobs.blade.php
@@ -8,10 +8,76 @@
         <div id="jobsImgPage" class="parallax">
             <h1 class="title-jumbo-secondary">Meus trabalhos</h1>
         </div>
-        <div class="container py-3">
-            <p class="content">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-            <img class="img-fluid" src="../img/material/sobrancelha.jpg" alt="titulo sobre imagem">
+    </section>
+    <section>
+        <div id="jobsImgPage2" class="parallax"></div>
+        <div class="container row mx-auto py-3">
+            <p class="content align-self-center col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12 my-3">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            <figure class="align-self-center col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12 my-3">
+                <img class="img-fluid" src="../img/jobs/workshop-para-profissionais.jpg" alt="Workshop para profissionais.">
+                <figcaption class="font-italic text-center small">Workshop para profissionais</figcaption>
+            </figure>
         </div>
     </section>
+
+    <section>
+        <!-- Trigger the Modal -->
+        <div class="row pt-2 mr-0">
+            <img class="job-img col-xl-2 col-lg-2 col-md-4 col-sm-4 col-12 px-0" src="img/jobs/cursos-de-auto-maquiagem.jpg" alt="Cursos de auto maquiagem">
+            <img class="job-img col-xl-2 col-lg-2 col-md-4 col-sm-4 col-6 px-0" src="img/jobs/atendimento-exclusivo-durante-eventos-debutante-event-care.jpg" alt="Atendimento exclusivo durante eventos debutante event care">
+            <img class="job-img col-xl-2 col-lg-2 col-md-4 col-sm-4 col-6 px-0" src="img/jobs/cabelo-laque.jpg" alt="Cabelo laque">
+            <img class="job-img col-xl-2 col-lg-2 col-md-4 col-sm-4 col-6 px-0" src="img/jobs/cursos-de-auto-maquiagem-pratica.jpg" alt="Cursos de auto maquiagem pratica">
+            <img class="job-img col-xl-2 col-lg-2 col-md-4 col-sm-4 col-6 px-0" src="img/jobs/cursos-de-postura-e-comportamento-profissional.jpg" alt="Cursos de postura e comportamento profissional">
+            <img class="job-img col-xl-2 col-lg-2 col-md-4 col-sm-4 col-6 px-0" src="img/jobs/cursos-profissionais.jpg" alt="Cursos profissionais">
+            <img class="job-img col-xl-2 col-lg-2 col-md-4 col-sm-4 col-6 px-0" src="img/jobs/design-com-hena.jpg" alt="Design com hena">
+            <img class="job-img col-xl-2 col-lg-2 col-md-4 col-sm-4 col-6 px-0" src="img/jobs/design-de-sobrancelhas.jpg" alt="Design de sobrancelhas">
+            <img class="job-img col-xl-2 col-lg-2 col-md-4 col-sm-4 col-6 px-0" src="img/jobs/design-de-sobrancelhas-2.jpg" alt="Design de sobrancelhas">
+            <img class="job-img col-xl-2 col-lg-2 col-md-4 col-sm-4 col-6 px-0" src="img/jobs/design-e-maquiagem-masculina-para-eventos.jpg" alt="Design e maquiagem masculina para eventos">
+            <img class="job-img col-xl-2 col-lg-2 col-md-4 col-sm-4 col-6 px-0" src="img/jobs/design-masculino.jpg" alt="Design masculino">
+            <img class="job-img col-xl-2 col-lg-2 col-md-4 col-sm-4 col-6 px-0" src="img/jobs/design-personalizado-e-medicao-facial-com-paquimetro.jpg" alt="Design personalizado e medicao facial com paquimetro">
+            <img class="job-img col-xl-2 col-lg-2 col-md-4 col-sm-4 col-6 px-0" src="img/jobs/home-care-noiva.jpg" alt="Home care noiva">
+            <img class="job-img col-xl-2 col-lg-2 col-md-4 col-sm-4 col-6 px-0" src="img/jobs/madrinhas.jpg" alt="Madrinhas">
+            <img class="job-img col-xl-2 col-lg-2 col-md-4 col-sm-4 col-6 px-0" src="img/jobs/make-curso-expert-em-noivas.jpg" alt="Make curso expert em noivas">
+            <img class="job-img col-xl-2 col-lg-2 col-md-4 col-sm-4 col-6 px-0" src="img/jobs/make-e-hair-noiva.jpg" alt="Make e hair noiva">
+            <img class="job-img col-xl-2 col-lg-2 col-md-4 col-sm-4 col-6 px-0" src="img/jobs/make-e-hair-noiva-2.jpg" alt="Make e hair noiva">
+            <img class="job-img col-xl-2 col-lg-2 col-md-4 col-sm-4 col-6 px-0" src="img/jobs/make-e-hair-para-ensaios-fotograficos.jpg" alt="Make e hair para ensaios fotograficos">
+            <img class="job-img col-xl-2 col-lg-2 col-md-4 col-sm-4 col-6 px-0" src="img/jobs/make-em-curso-profissional.jpg" alt="Make em curso profissional">
+            <img class="job-img col-xl-2 col-lg-2 col-md-4 col-sm-4 col-6 px-0" src="img/jobs/make-noiva.jpg" alt="Make noiva">
+            <img class="job-img col-xl-2 col-lg-2 col-md-4 col-sm-4 col-6 px-0" src="img/jobs/make-para-evento.jpg" alt="Make para evento">
+            <img class="job-img col-xl-2 col-lg-2 col-md-4 col-sm-4 col-6 px-0" src="img/jobs/make-para-evento-diurno.jpg" alt="Make para evento diurno">
+            <img class="job-img col-xl-2 col-lg-2 col-md-4 col-sm-4 col-6 px-0" src="img/jobs/make-pele-negra.jpg" alt="Make pele negra">
+            <img class="job-img col-xl-2 col-lg-2 col-md-4 col-sm-4 col-6 px-0" src="img/jobs/making-off-noivas.jpg" alt="Making off noivas">
+            <img class="job-img col-xl-2 col-lg-2 col-md-4 col-sm-4 col-6 px-0" src="img/jobs/maquiagem-e-penteado-para-mae-dos-noivos.jpg" alt="Maquiagem e penteado para mae dos noivos">
+            <img class="job-img col-xl-2 col-lg-2 col-md-4 col-sm-4 col-6 px-0" src="img/jobs/microblading-fio-a-fio.jpg" alt="Microblanding fio a fio">
+            <img class="job-img col-xl-2 col-lg-2 col-md-4 col-sm-4 col-6 px-0" src="img/jobs/microblanding-fio-a-fio-semi-permanente-em-sobrancelhas-2.jpg" alt="Microblanding fio a fio">
+            <img class="job-img col-xl-2 col-lg-2 col-md-4 col-sm-4 col-6 px-0" src="img/jobs/micropgmentacao.jpg" alt="Micropgmentação">
+            <img class="job-img col-xl-2 col-lg-2 col-md-4 col-sm-4 col-6 px-0" src="img/jobs/micropgmentacao-2.jpg" alt="Micropgmentação">
+            <img class="job-img col-xl-2 col-lg-2 col-md-4 col-sm-4 col-6 px-0" src="img/jobs/noiva-by-me.jpg" alt="Noiva by me">
+            <img class="job-img col-xl-2 col-lg-2 col-md-4 col-sm-4 col-6 px-0" src="img/jobs/noiva-classica.jpg" alt="Noiva clássica">
+            <img class="job-img col-xl-2 col-lg-2 col-md-4 col-sm-4 col-6 px-0" src="img/jobs/noiva-classica-2.jpg" alt="Noiva clássica">
+            <img class="job-img col-xl-2 col-lg-2 col-md-4 col-sm-4 col-6 px-0" src="img/jobs/pacote-penteado-mais-make.jpg" alt="Pacote penteado mais make">
+            <img class="job-img col-xl-2 col-lg-2 col-md-4 col-sm-4 col-6 px-0" src="img/jobs/penteado-noiva.jpg" alt="Penteado noiva">
+            <img class="job-img col-xl-2 col-lg-2 col-md-4 col-sm-4 col-6 px-0" src="img/jobs/penteado-para-festa.jpg" alt="Penteado para festa">
+            <img class="job-img col-xl-2 col-lg-2 col-md-4 col-sm-4 col-6 px-0" src="img/jobs/teste-de-noiva.jpg" alt="Teste de noiva">
+            <img class="job-img col-xl-2 col-lg-2 col-md-4 col-sm-4 col-6 px-0" src="img/jobs/make-noiva.jpg" alt="Make noite">
+            <img class="job-img col-xl-2 col-lg-2 col-md-4 col-sm-4 col-6 px-0" src="img/jobs/workshops-de-atualizacoes-profissionais.jpg" alt="Workshops de atualizacoes profissionais">
+            <!-- Preciso adicionar só mais 1 foto, trocando pela a de baixo -->
+            <img class="job-img col-xl-2 col-lg-2 col-md-4 col-sm-4 col-6 px-0" src="img/jobs/workshop-para-profissionais.jpg" alt="Workshop para profissionais">
+            <img class="job-img col-xl-2 col-lg-2 col-md-4 col-sm-4 col-6 px-0" src="img/jobs/workshop-para-profissionais.jpg" alt="Workshop para profissionais">
+            <img class="job-img col-xl-2 col-lg-2 col-md-4 col-sm-4 col-6 px-0" src="img/jobs/workshop-para-profissionais.jpg" alt="Workshop para profissionais">
+            <img class="job-img col-xl-2 col-lg-2 col-md-4 col-sm-4 col-12 px-0" src="img/jobs/cursos-de-auto-maquiagem.jpg" alt="Cursos de auto maquiagem">
+        </div>
+        <!-- The Modal -->
+        <div id="myModal" class="modal">
+            <!-- The Close Button -->
+            <span class="close">&times;</span>
+            <!-- Modal Content (The Image) -->
+            <img class="modal-content" id="imgRecepient">
+            <!-- Modal Caption (Image Text) -->
+            <div id="caption"></div>
+        </div>
+    </section>
+
+    @include('home.contact')
 
 @endsection
