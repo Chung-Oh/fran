93b530727c9e8b57e0cf7108e57b724ae7350359
Daniel Chung <danielvchung@hotmail.com>
2019-12-26 00:15:55 -0300

    some adjustments on the page about

--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -20,42 +20,42 @@
                 <img class="img-thumbnail" src="img/jobs/make-e-hair-noiva.jpg" alt="Maquiagem e penteado para noiva" style="height: 250px;width: 250px;border-radius: 50%;">
                 <p class="h4 text-center pt-2">Casamento</p>
                 <div class="line"></div>
-                <p class="content font-italic mx-5">Falar sobre serviço de casamento. Falar sobre serviço de casamento. Falar sobre serviço de casamento.</p>
+                <p class="content font-italic mx-3">Falar sobre serviço de casamento. Falar sobre serviço de casamento. Falar sobre serviço de casamento.</p>
             </div>
             <!-- Debutante -->
             <div class="container text-center col-xl-4 col-lg-4 col-md-4 col-sm-6 col-12 py-3">
                 <img class="img-thumbnail" src="img/material/debutante.jpg" alt="Maquiagem e penteado para debutante" style="height: 250px;width: 250px;border-radius: 50%;">
                 <p class="h4 text-center pt-2">Debutante</p>
                 <div class="line"></div>
-                <p class="content font-italic mx-5">Falar sobre serviço de debutante. Falar sobre serviço de debutante. Falar sobre serviço de debutante.</p>
+                <p class="content font-italic mx-3">Falar sobre serviço de debutante. Falar sobre serviço de debutante. Falar sobre serviço de debutante.</p>
             </div>
             <!-- Eventos em geral -->
             <div class="container text-center col-xl-4 col-lg-4 col-md-4 col-sm-6 col-12 py-3">
                 <img class="img-thumbnail" src="img/material/eventos-geral.jpg" alt="Maquiagem e penteado para eventos em geral" style="height: 250px;width: 250px;border-radius: 50%;">
                 <p class="h4 text-center pt-2">Eventos em geral</p>
                 <div class="line"></div>
-                <p class="content font-italic mx-5">Falar sobre serviço de eventos em geral. Falar sobre serviço de eventos em geral. Falar sobre serviço de eventos em geral.</p>
+                <p class="content font-italic mx-3">Falar sobre serviço de eventos em geral. Falar sobre serviço de eventos em geral. Falar sobre serviço de eventos em geral.</p>
             </div>
             <!-- Make balada -->
             <div class="container text-center col-xl-4 col-lg-4 col-md-4 col-sm-6 col-12 py-3">
                 <img class="img-thumbnail" src="img/material/make-balada.jpg" alt="Maquiagem e penteado para balada" style="height: 250px;width: 250px;border-radius: 50%;">
                 <p class="h4 text-center pt-2">Make balada</p>
                 <div class="line"></div>
-                <p class="content font-italic mx-5">Falar sobre serviço de make balada. Falar sobre serviço de make balada. Falar sobre serviço de make balada.</p>
+                <p class="content font-italic mx-3">Falar sobre serviço de make balada. Falar sobre serviço de make balada. Falar sobre serviço de make balada.</p>
             </div>
             <!-- Make social -->
             <div class="container text-center col-xl-4 col-lg-4 col-md-4 col-sm-6 col-12 py-3">
                 <img class="img-thumbnail" src="img/material/make-social.jpg" alt="Maquiagem e penteado social" style="height: 250px;width: 250px;border-radius: 50%;">
                 <p class="h4 text-center pt-2">Make social</p>
                 <div class="line"></div>
-                <p class="content font-italic mx-5">Falar sobre serviço de make social. Falar sobre serviço de make social. Falar sobre serviço de make social.</p>
+                <p class="content font-italic mx-3">Falar sobre serviço de make social. Falar sobre serviço de make social. Falar sobre serviço de make social.</p>
             </div>
             <!-- Make ensaio fotográfico -->
             <div class="container text-center col-xl-4 col-lg-4 col-md-4 col-sm-6 col-12 py-3">
                 <img class="img-thumbnail" src="img/material/make-ensaio-fotografico.jpg" alt="Maquiagem e penteado para ensaio fotográfico" style="height: 250px;width: 250px;border-radius: 50%;">
                 <p class="h4 text-center pt-2">Make ensaio fotográfico</p>
                 <div class="line"></div>
-                <p class="content font-italic mx-5">Falar sobre serviço de make ensaio fotográfico. Falar sobre serviço de make ensaio fotográfico. Falar sobre serviço de make ensaio fotográfico.</p>
+                <p class="content font-italic mx-3">Falar sobre serviço de make ensaio fotográfico. Falar sobre serviço de make ensaio fotográfico. Falar sobre serviço de make ensaio fotográfico.</p>
             </div>
         </div>
     </section>
