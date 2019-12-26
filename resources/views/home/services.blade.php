38ab1297cba67c8afb2bdfb1c757bd2b6a6e114b
Daniel Chung <danielvchung@hotmail.com>
2019-12-26 17:00:14 -0300

    adding new service in home and services page

--- resources/views/home/services.blade.php
+++ resources/views/home/services.blade.php
@@ -32,6 +32,16 @@
             <div class="line"></div>
             <p class="content font-italic pb-2">Colocar aqui sobre serviços para Make ensaio fotográfico. Colocar aqui sobre serviços para Make ensaio fotográfico. Colocar aqui sobre serviços para Make ensaio fotográfico. </p>
         </div>
+        <!-- <div class="py-3 col-xl-4 col-lg-4 col-md-6 col-sm-6 col-12">
+            <h6 class="service-type pt-2">Curso de auto maquiagem</h6>
+            <div class="line"></div>
+            <p class="content font-italic pb-2">Colocar aqui sobre serviços para Curso de auto maquiagem. Colocar aqui sobre serviços para Curso de auto maquiagem. Colocar aqui sobre serviços para Curso de auto maquiagem. </p>
+        </div> -->
+        <div class="py-3 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+            <h6 class="service-type pt-2">Curso de auto maquiagem</h6>
+            <div class="line"></div>
+            <p class="last-service content font-italic mx-auto pb-2"">Colocar aqui sobre serviços para Curso de auto maquiagem. Colocar aqui sobre serviços para Curso de auto maquiagem. Colocar aqui sobre serviços para Curso de auto maquiagem. </p>
+        </div>
     </div>
     <div class="container text-center py-5 mb-5">
         <a class="btn-page" href="/services">Agende já.</a>
